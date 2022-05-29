2d24f9e8e1ffca5af8efeb7370063d125c8c253a
Philipp Oberdiek <philipp.oberdiek@cs.tu-dortmund.de>
2022-05-29 11:59:25 +0200

    Format and add type hints

--- pirate_add_default_time.py
+++ pirate_add_default_time.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python
 from __future__ import print_function
-from datetime import time
+
+from datetime import datetime, time
+
 from tasklib import Task, local_zone
 
-DEFAULT_TIME = time(19,0,0)  # Your wanted default time
+DEFAULT_TIME = time(19, 0, 0)  # Your wanted default time
 
-def is_local_midnight(timestamp):
-    return timestamp.astimezone(local_zone).time() == time(0,0,0)
 
-def set_default_time(timestamp):
+def is_local_midnight(timestamp: datetime) -> bool:
+    return timestamp.astimezone(local_zone).time() == time(0, 0, 0)
+
+
+def set_default_time(timestamp: datetime) -> datetime:
     return timestamp.astimezone(local_zone).replace(
         hour=DEFAULT_TIME.hour,
         minute=DEFAULT_TIME.minute,
         second=DEFAULT_TIME.second,
-        )
+    )
 
-def hook_default_time(task):
-    if task['due'] and is_local_midnight(task['due']):
-        task['due'] = set_default_time(task['due'])
+
+def hook_default_time(task: Task) -> None:
+    if task["due"] and is_local_midnight(task["due"]):
+        task["due"] = set_default_time(task["due"])
         print("Default due time has been set.")
