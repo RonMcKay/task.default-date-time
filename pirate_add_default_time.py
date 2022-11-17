7b62dbfadb0e04deb198af5f4a49ff64fd0b3706
Philipp Oberdiek <philipp.oberdiek@cs.tu-dortmund.de>
2022-11-17 16:41:36 +0100

    fix: Get local timezone from datetime library
    tasklib removed `local_zone` in version 2.5.1

--- pirate_add_default_time.py
+++ pirate_add_default_time.py
@@ -3,17 +3,17 @@ from __future__ import print_function
 
 from datetime import datetime, time
 
-from tasklib import Task, local_zone
+from tasklib import Task
 
 DEFAULT_TIME = time(19, 0, 0)  # Your wanted default time
 
 
 def is_local_midnight(timestamp: datetime) -> bool:
-    return timestamp.astimezone(local_zone).time() == time(0, 0, 0)
+    return timestamp.astimezone(datetime.now().astimezone()).time() == time(0, 0, 0)
 
 
 def set_default_time(timestamp: datetime) -> datetime:
-    return timestamp.astimezone(local_zone).replace(
+    return timestamp.astimezone(datetime.now().astimezone()).replace(
         hour=DEFAULT_TIME.hour,
         minute=DEFAULT_TIME.minute,
         second=DEFAULT_TIME.second,
